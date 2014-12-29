477e4b026ad3f69ac97e5c7ed2a715a6ff7ea00e
William McRae <william.mcrae@gmail.com>
2014-12-29 11:47:01 +1100

    removing old site

--- config/deploy/prod.rb
+++
@@ -1,5 +0,0 @@
-# Production deployment config
-set :deploy_to, "/hsphere/local/home/onwhat/mountbeautypharmacy_prod"
-set :branch, 'master'
-role :app, "quadra"
-set :app_path, "#{release_path}/app"
--- push.sh
+++
@@ -1,1 +0,0 @@
-rsync -avz -e 'ssh -p2222' --delete --exclude '.DS_Store' static/* deployer@host.onwhat.net:/var/www/mbp
